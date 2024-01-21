5869e83df9f5f9febfe0fd7f340767023993e010
Otavio Luiz <95648653+TavinhoNZT@users.noreply.github.com>
2024-01-21 15:00:03 -0300

    att

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def blog(request):
-    print('Blog do App')
-    return HttpResponse('Blog do App 1')
+    print('blog')
+    return render(
+        request,
+        'blog/index.html'
+        )
